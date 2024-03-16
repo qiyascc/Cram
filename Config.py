d9630a21ef9e611a9a2c56724d165d7afed7a72a
Qiyas <107604130+qiyascc@users.noreply.github.com>
2024-03-16 20:09:11 +0400

    Add files via upload

---
+++ Config.py
@@ -0,0 +1,31 @@
+from pyrogram import Client
+from pyromod import listen
+
+API_ID = 0
+API_HASH = "x"
+BOT_TOKEN = "x"
+
+app = Client("Cram", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+
+DATABASE_URL = "x"
+
+# Varsayılan admin id'si
+DEFAULT_ADMIN = 6970363662
+# Adminlerin id'leri
+ADMINS = []
+# Derslerin isimleri
+LESSONS = []
+# Grup id'si
+GROUP_ID = -1002041382483
+# Piskoloji seçenekleri
+PYSCHOLOGY = []
+# Kullancıların Uzun soru açıklamaları tutmak için bir sözlük , uzun cümleler calbackdtada tutulamaz
+TEXT = {}
+# Kullanıcı listesi
+USERS = []
+# Black list
+BLACK_LIST = []
+#Others 
+OTHERS = {}
+# Destek ekibi
+SUPPORT = "SpicaCrew"
